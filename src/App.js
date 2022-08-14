23a562f99d8b410f62ce0de5a8a80be51174bf57
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2022-08-14 13:14:56 -0300

    resolute url

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <BrowserRouter className="App">
+    <HashRouter className="App">
       <Routes></Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
