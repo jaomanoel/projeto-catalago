982c769a063d7e74d88028df8b8ac66da536d2d1
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2022-08-14 14:35:31 -0300

    resolute url

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import {React, useState} from "react";
 import Axios from "axios";
 import { Link } from "react-router-dom";
-import { Axios } from "Axios";
+import { Axios } from "axios";
 import Main from "./Main";
 
 const Header = () => {
